e7187d0ced1f7b02f1fb8a1ed997c02ee86baea2
CYGNET\nrmandowara <shahnihir.293@gmail.com>
2016-12-23 11:00:30 +0530

    initial commit - SignalR is commit but the app is still pretty lame :(

---
+++ SignalR/Scripts/SignalR.js
@@ -0,0 +1,11 @@
+﻿$.connection.hub.start()
+            .done(function () {
+                console.log("It Worked!");
+                $.connection.myHub.server.annouce("Connected!");
+            })
+            .fail(function () { alert("Failed"); })
+
+$.connection.myHub.client.annouce = function (message)
+{
+    $('#welcome-messages').append(message + "<br/>");
+}
---
+++ SignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR.Startup))]
+namespace SignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+        }
+    }
+}
